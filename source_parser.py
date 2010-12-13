5da85296179adef381bb3c55b532d335222f1396
Jacob O'Donnell <jacobodonnell@gmail.com>
2010-12-13 00:51:31 -0500

    small refactor, extract method

--- source_parser.py
+++ source_parser.py
@@ -10,18 +10,22 @@ class SourceParser():
         self._get_classes()
 
     def _get_classes(self):
-        walked_classes = compiler.walk(self.source, ClassVisitor())
+        class_nodes = compiler.walk(self.source, ClassVisitor()).class_nodes
 
         num_props = []
-        for class_node in walked_classes.class_nodes:
+        for class_node in class_nodes:
             num_props.append(compiler.walk(class_node, PropertyVisitor()).num_props)
 
+        self._create_source_classes(class_nodes, num_props)
+        self._inheritance(class_nodes)
+
+    def _create_source_classes(self, class_nodes, num_props):
         self.source_classes = []
-        for source_class in zip(walked_classes.class_nodes, num_props):
+        for source_class in zip(class_nodes, num_props):
             self.source_classes.append(SourceClass(source_class[0].name, source_class[1]))
 
-
-        for class_node in walked_classes.class_nodes:
+    def _inheritance(self, class_nodes):
+        for class_node in class_nodes:
             if class_node.asList()[1]:
                 parent_class_name = class_node.asList()[1].name
 
@@ -34,12 +38,12 @@ class SourceParser():
                     if source_class.get_name() == parent_class_name:
                         this_class.set_num_props(source_class.get_num_properties() + this_class.get_num_properties())
 
+
     def get_classes(self):
         return self.source_classes
 
 class ClassVisitor(compiler.visitor.ASTVisitor):
     def __init__(self):
-        self.class_names = []
         self.class_nodes = []
 
     def visitClass(self, node):
